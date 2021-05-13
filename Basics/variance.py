8b143c553e1bd9b13c822a59e0a1d6c690a09412
mafm9 <miguelafm2@gmail.com>
2021-05-13 09:22:13 -0500

    finished variance problem

--- Basics/variance.py
+++ Basics/variance.py
@@ -1,7 +1,11 @@
-lowerbound = input("Please enter lower bound of range: ")
-upperbound = input("Please enter upper bound of range: ")
-variance = input("please enter variance: ")
-number = input("Please enter number")
+lowerbound = int(input("Please enter lower bound of range: "))
+upperbound = int(input("Please enter upper bound of range: "))
+variance = int(input("please enter variance: "))
+number = int(input("Please enter number: "))
 
+if (number < lowerbound + variance and number > lowerbound - variance) or (number > upperbound - variance and number < upperbound + variance):
+    print(f"{number} is within variance of range limits")
+else:
+    print(f"{number} is out of variance of range limits")
 
 
